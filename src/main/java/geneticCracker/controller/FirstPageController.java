ad3c7b4300f586ff9a2a55086f8b6409ac7519d5
KSedlak <krzysztof.sedlak@hotmail.com>
2015-11-07 09:27:00 +0100

    x

--- src/main/java/geneticCracker/controller/FirstPageController.java
+++ src/main/java/geneticCracker/controller/FirstPageController.java
@@ -1,7 +1,5 @@
 package geneticCracker.controller;
 
-
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -47,361 +45,357 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 
-
 @Component
 public class FirstPageController {
 
+	@FXML
+	Tab crypterTab;
+	@FXML
+	TextArea plainTextArea;
+	@FXML
+	ComboBox<String> cipherChooser;
+	@FXML
+	ComboBox<Integer> keyLengthChooser;
+	@FXML
+	Button cryptButton;
+	@FXML
+	TextArea cryptetTextArea;
+	@FXML
+	TextField keyField;
+	@FXML
+	TextArea decryptedAreaFirstTab;
+	@FXML
+	Label markLabel;
+	@FXML
+	ComboBox markFunctions;
+	@FXML
+	Button markDecryptedText;
+	@FXML
+	ListView forWhatPointList;
+
+	@Autowired
+	TranspositionCrypter tCrypter;
+	@Autowired
+	LanguageBean language;
+	@Autowired
+	SubstitiutionCrypter sCrypter;
+
+	@FXML
+	ListView<String> commonWords;
+	@FXML
+	ComboBox<String> languageChoosen;
+	@FXML
+	ComboBox<Integer> ngramLength;
+	@FXML
+	Button showButton;
+
+	@Autowired
+	LanguageAnalyzer analyzer;
+
+	private ObservableList<NgramsTableRow> ngramsObservableList = FXCollections.observableArrayList();
+
+	@FXML
+	TableView<NgramsTableRow> ngramsTable;
+	@FXML
+	TableColumn<NgramsTableRow, Integer> ngramQtyColumn;
+	@FXML
+	TableColumn<NgramsTableRow, String> ngramColumn;
+	@FXML
+	TextArea decryptedTEXT;
+	@FXML
+	TextArea textToDBreak;
+	@FXML
+	Button Start;
+	@FXML
+	TextField popSizeField;
+	@FXML
+	TextField decodedKeyField;
+	@FXML
+	Label bestFitMark;
+	@FXML
+	Button nextGenearationNumber;
+	@FXML
+	Label generationNumber;
+	@FXML
+	TextField geneartionsNumberInput;
+
+	private Task task;
+	private Creature best;
+
+	@FXML
+	ProgressBar progressBar;
+	@Autowired
+	World world;
+
+	protected Text decoded;
+
+	@FXML
+	ComboBox fitnessByCombo;
+	List<FitnessMaker> fitnessFunctions;
+	@FXML
+	ListView pointsFor;
+	@Autowired
+	FitnesserOnlyFrequentWord fitnesserOnlyFrequentWord;
+	@Autowired
+	FitnesserOnlyNgrams fitnesserOnlyNgrams;
+
+	private FitnessMaker fitnesser;
+
+	private ObservableList<String> pointsForList;
+
+	@FXML
+	private void initialize() {
+
+		plainTextArea.setWrapText(true);
+		cryptetTextArea.setWrapText(true);
+
+		cipherChooser.getItems().add("Podstawieniowy");
+		cipherChooser.getItems().add("Transpozycyjny");
 
-		@FXML Tab crypterTab;
-		@FXML TextArea plainTextArea;
-		@FXML ComboBox<String> cipherChooser;
-		@FXML ComboBox<Integer> keyLengthChooser;
-		@FXML Button cryptButton;
-		@FXML TextArea cryptetTextArea;
-		@FXML TextField keyField;
-		@FXML TextArea decryptedAreaFirstTab;
-		@FXML Label markLabel;
-		@FXML ComboBox markFunctions;
-		@FXML Button markDecryptedText;
+		cipherChooser.getSelectionModel().select(0);
 
-		@Autowired TranspositionCrypter tCrypter;
-		@Autowired LanguageBean language;
-		@Autowired SubstitiutionCrypter sCrypter;
+		keyLengthChooser.getItems().addAll(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+				24, 25, 26, 27, 28, 29, 30);
+		keyLengthChooser.getSelectionModel().select(3);
 
+		markFunctions.getItems().add("By Word Freq");
+		markFunctions.getItems().add("By Ngram Freq");
+		markFunctions.getSelectionModel().select(0);
+		initTextFromScratch();
 
-		@FXML ListView<String> commonWords;
-		@FXML ComboBox<String> languageChoosen;
-		@FXML ComboBox<Integer> ngramLength;
-		@FXML Button showButton;
+		// tab2
 
-		@Autowired LanguageAnalyzer analyzer;
+		languageChoosen.getItems().add("Polski");
+		languageChoosen.getItems().add("Angielski");
 
-		private ObservableList<NgramsTableRow> ngramsObservableList =FXCollections.observableArrayList();
+		languageChoosen.getSelectionModel().select(1);
 
-		@FXML TableView<NgramsTableRow> ngramsTable;
-		@FXML TableColumn<NgramsTableRow,Integer> ngramQtyColumn;
-		@FXML TableColumn<NgramsTableRow, String> ngramColumn;
-		@FXML TextArea decryptedTEXT;
-		@FXML TextArea textToDBreak;
-		@FXML Button Start;
-		@FXML TextField popSizeField;
-		@FXML TextField decodedKeyField;
-		@FXML Label bestFitMark;
-		@FXML Button nextGenearationNumber;
-		@FXML Label generationNumber;
-		@FXML TextField geneartionsNumberInput;
+		ngramLength.getItems().addAll(2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-		private Task task;
-	  	private Creature best;
+		ngramLength.getSelectionModel().select(1);
 
-		@FXML ProgressBar progressBar;
-		@Autowired
-		World world;
+		ngramsTable.setItems(ngramsObservableList);
 
-		protected Text decoded;
+		ngramColumn.setCellValueFactory(cellData -> cellData.getValue().getValue());
+		ngramQtyColumn.setCellValueFactory(cellData -> cellData.getValue().getQuantity().asObject());
 
-		@FXML ComboBox fitnessByCombo;
-		List<FitnessMaker> fitnessFunctions;
-		@FXML ListView pointsFor;
-		@Autowired
-		FitnesserOnlyFrequentWord fitnesserOnlyFrequentWord;
-		@Autowired
-		FitnesserOnlyNgrams fitnesserOnlyNgrams;
+		// tab3
 
-		private FitnessMaker fitnesser;
+		fitnessByCombo.getItems().addAll(markFunctions.getItems());
 
-		private ObservableList<String> pointsForList;
+		fitnessByCombo.getSelectionModel().select(1);
 
+		fitnessFunctions = new ArrayList<FitnessMaker>();
+		pointsForList = FXCollections.observableArrayList();
 
-		@FXML
-		private void initialize() {
+		pointsFor.setItems(pointsForList);
+		forWhatPointList.setItems(pointsForList);
 
-			plainTextArea.setWrapText(true);
-			cryptetTextArea.setWrapText(true);
-
-			cipherChooser.getItems().add("Podstawieniowy");
-			cipherChooser.getItems().add("Transpozycyjny");
-
-			cipherChooser.getSelectionModel().select(0);
-
-			keyLengthChooser.getItems().addAll(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30);
-			keyLengthChooser.getSelectionModel().select(3);
-
-
-			markFunctions.getItems().add("By Word Freq");
-			markFunctions.getItems().add("By Ngram Freq");
-			markFunctions.getSelectionModel().select(0);
-			initTextFromScratch();
-
-			//tab2
-
-			languageChoosen.getItems().add("Polski");
-			languageChoosen.getItems().add("Angielski");
-
-			languageChoosen.getSelectionModel().select(1);
-
-			ngramLength.getItems().addAll(2,3,4,5,6,7,8,9,10);
-
-			ngramLength.getSelectionModel().select(1);
-
-			ngramsTable.setItems(ngramsObservableList);
-
-			ngramColumn.setCellValueFactory(cellData -> cellData.getValue().getValue());
-			ngramQtyColumn.setCellValueFactory(cellData -> cellData.getValue().getQuantity().asObject());
-
-			//tab3
-
-			fitnessByCombo.getItems().addAll(markFunctions.getItems());
-
-
-			fitnessByCombo.getSelectionModel().select(1);
-
-			fitnessFunctions=new ArrayList<FitnessMaker>();
-			pointsForList=FXCollections.observableArrayList();
-
-
-
-
-
-
-		}
-
-		private void initTextFromScratch() {
-			plainTextArea.setText(fileToString.getFileFromDirectory("text/subject.txt"));
-
-
-		}
-
-		@FXML public void crypt(ActionEvent event) {
-
-
-			if(cipherChooser.getSelectionModel().getSelectedIndex()==1){
-				int length=keyLengthChooser.getSelectionModel().getSelectedItem();
-					TranspositionKey key= new TranspositionKey(length);
-					Text t=new Text(plainTextArea.getText());
-					tCrypter.crypt(t, key);
-					keyField.setText(key.getKeyString());
-					cryptetTextArea.setText(t.getContentOfText());
-			}
-
-			if(cipherChooser.getSelectionModel().getSelectedIndex()==0){
-					Language lang=language.getLanguageFromString(plainTextArea.getText());
-					Text t=new Text(plainTextArea.getText(), lang);
-					SubstitutionKey key=new SubstitutionKey(lang);
-					sCrypter.crypt(t, key);
-					keyField.setText(key.getKeyString());
-					cryptetTextArea.setText(t.getContentOfText());
-
-
-
-			}
-
-			textToDBreak.setText(cryptetTextArea.getText());
-		}
-
-		@FXML public void decrypt(ActionEvent event) {
-
-
-			if(keyField.getText().contains("1")){
-				Text t=new Text(cryptetTextArea.getText());
-				TranspositionKey key= new TranspositionKey(keyField.getText());
-				tCrypter.decrypt(t, key);
-				decryptedAreaFirstTab.setText(t.getContentOfText());
-				cipherChooser.getSelectionModel().select(1);
-				keyLengthChooser.getSelectionModel().select(key.getKey().length-4);
-			}
-
-			if(keyField.getText().contains("A")){
-				Language lang = language.getEnglish();
-				SubstitutionKey key=new SubstitutionKey(keyField.getText().split(","));
-				if(keyField.getText().contains("Ż")){
-				lang=language.getPolish();
-				}
-
-				Text t=new Text(cryptetTextArea.getText(),lang);
-
-				Text dec=sCrypter.decrypt(t, key);
-				decryptedAreaFirstTab.setText(dec.getContentOfText());
-				cipherChooser.getSelectionModel().select(0);
-			}
-		}
-
-		@FXML public void analyzeLanguage(ActionEvent event) {
-
-			int length=ngramLength.getSelectionModel().getSelectedItem();
-
-			ngramsTable.getItems().clear();
-
-
-			String lang="";
-
-			if(languageChoosen.getSelectionModel().getSelectedItem().equals("Polski")){
-				lang="pl";
-			}
-
-			if(languageChoosen.getSelectionModel().getSelectedItem().equals("Angielski")){
-				lang="eng";
-			}
-			commonWords.getItems().clear();
-			try {
-				commonWords.getItems().addAll(analyzer.getMostFrequentWords(lang, 50));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			lang="text/learn/"+lang+"/txt";//make dir
-
-			LinkedHashMap<String, Integer> map=analyzer.getGlobalFrequencyMap(lang, length);
-			for(String s:map.keySet()){
-				ngramsObservableList.add(
-						new NgramsTableRow(
-								new SimpleStringProperty(s.replace(" ","_")),
-								new SimpleIntegerProperty(map.get(s))));
-			}
-
-		}
-
-		@FXML public void startDecodingByGenetic(ActionEvent event) {
-			initWorld();
-			initializeFitnessFunctions();
-
-			world.start(
-					Integer.parseInt(popSizeField.getText()),
-					fitnessFunctions.get(fitnessByCombo.getSelectionModel().getSelectedIndex())
-					);
-
-			task = new Task<Void>() {
-			    @Override public Void call() {
-			        final int max =	Integer.parseInt(geneartionsNumberInput.getText());
-			        int i;
-			        for (i = 1; i <= max; i++) {
-			        	world.generate();
-			        	System.out.println(world.getBestCreatureOnWholeWorld().getDna().getKeyString()+" "+world.getBestCreatureOnWholeWorld().getMark());
-			            updateProgress(i, max);
-
-			        }
-			        return null;
-			    }
-
-			    @Override
-			    protected void succeeded() {
-			     	Platform.runLater(new Runnable() {
-		    		   @Override
-	        		   public void run() {
-	        		         best=world.getBestCreatureOnWholeWorld();
-	 						decoded=crypterBasedDna(best).decrypt(new Text(textToDBreak.getText(),best.getText().getLanguage()), best.getDna());
-	 						decryptedTEXT.setText(decoded.getContentOfText());
-	 						bestFitMark.setText(best.getMark()+"");
-	 						decodedKeyField.setText(best.getDna().getKeyString());
-	 						generationNumber.setText(world.getWorldGeneration()+"");
-	 						updatePointsFor(best);
-	        		   }
-	        		});
-			    	super.succeeded();
-
-
-			    }
-			};
-			progressBar.progressProperty().bind(task.progressProperty());
-
-			new Thread(task).start();
-
-
-
-
-
-		}
-
-	private void updatePointsFor(Creature c){
-		pointsFor.getItems().clear();
-		pointsFor.getItems().addAll(c.getPoints().keySet());
 	}
 
-		private void initializeFitnessFunctions() {
-			fitnessFunctions.clear();
-			fitnessFunctions.add(fitnesserOnlyFrequentWord);
-			fitnessFunctions.add(fitnesserOnlyNgrams);
+	private void initTextFromScratch() {
+		plainTextArea.setText(fileToString.getFileFromDirectory("text/subject.txt"));
+
+	}
+
+	@FXML
+	public void crypt(ActionEvent event) {
+
+		if (cipherChooser.getSelectionModel().getSelectedIndex() == 1) {
+			int length = keyLengthChooser.getSelectionModel().getSelectedItem();
+			TranspositionKey key = new TranspositionKey(length);
+			Text t = new Text(plainTextArea.getText());
+			tCrypter.crypt(t, key);
+			keyField.setText(key.getKeyString());
+			cryptetTextArea.setText(t.getContentOfText());
 		}
 
+		if (cipherChooser.getSelectionModel().getSelectedIndex() == 0) {
+			Language lang = language.getLanguageFromString(plainTextArea.getText());
+			Text t = new Text(plainTextArea.getText(), lang);
+			SubstitutionKey key = new SubstitutionKey(lang);
+			sCrypter.crypt(t, key);
+			keyField.setText(key.getKeyString());
+			cryptetTextArea.setText(t.getContentOfText());
 
+		}
 
+		textToDBreak.setText(cryptetTextArea.getText());
+	}
 
+	@FXML
+	public void decrypt(ActionEvent event) {
 
+		if (keyField.getText().contains("1")) {
+			Text t = new Text(cryptetTextArea.getText());
+			TranspositionKey key = new TranspositionKey(keyField.getText());
+			tCrypter.decrypt(t, key);
+			decryptedAreaFirstTab.setText(t.getContentOfText());
+			cipherChooser.getSelectionModel().select(1);
+			keyLengthChooser.getSelectionModel().select(key.getKey().length - 4);
+		}
 
-		private Crypter crypterBasedDna(Creature c){
-			if(c.getDna() instanceof SubstitutionKey){
-				return sCrypter;
+		if (keyField.getText().contains("A")) {
+			Language lang = language.getEnglish();
+			SubstitutionKey key = new SubstitutionKey(keyField.getText().split(","));
+			if (keyField.getText().contains("Ż")) {
+				lang = language.getPolish();
+			}
+
+			Text t = new Text(cryptetTextArea.getText(), lang);
+
+			Text dec = sCrypter.decrypt(t, key);
+			decryptedAreaFirstTab.setText(dec.getContentOfText());
+			cipherChooser.getSelectionModel().select(0);
+		}
+	}
+
+	@FXML
+	public void analyzeLanguage(ActionEvent event) {
+
+		int length = ngramLength.getSelectionModel().getSelectedItem();
+
+		ngramsTable.getItems().clear();
+
+		String lang = "";
+
+		if (languageChoosen.getSelectionModel().getSelectedItem().equals("Polski")) {
+			lang = "pl";
+		}
+
+		if (languageChoosen.getSelectionModel().getSelectedItem().equals("Angielski")) {
+			lang = "eng";
+		}
+		commonWords.getItems().clear();
+		try {
+			commonWords.getItems().addAll(analyzer.getMostFrequentWords(lang, 50));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		lang = "text/learn/" + lang + "/txt";// make dir
+
+		LinkedHashMap<String, Integer> map = analyzer.getGlobalFrequencyMap(lang, length);
+		for (String s : map.keySet()) {
+			ngramsObservableList.add(new NgramsTableRow(new SimpleStringProperty(s.replace(" ", "_")),
+					new SimpleIntegerProperty(map.get(s))));
+		}
+
+	}
+
+	@FXML
+	public void startDecodingByGenetic(ActionEvent event) {
+		initWorld();
+		initializeFitnessFunctions();
+
+		world.start(Integer.parseInt(popSizeField.getText()),
+				fitnessFunctions.get(fitnessByCombo.getSelectionModel().getSelectedIndex()));
+
+		task = new Task<Void>() {
+			@Override
+			public Void call() {
+				final int max = Integer.parseInt(geneartionsNumberInput.getText());
+				int i;
+				for (i = 1; i <= max; i++) {
+					world.generate();
+					System.out.println(world.getBestCreatureOnWholeWorld().getDna().getKeyString() + " "
+							+ world.getBestCreatureOnWholeWorld().getMark());
+					updateProgress(i, max);
+
+				}
+				return null;
+			}
+
+			@Override
+			protected void succeeded() {
+				Platform.runLater(new Runnable() {
+					@Override
+					public void run() {
+						best = world.getBestCreatureOnWholeWorld();
+						decoded = crypterBasedDna(best)
+								.decrypt(new Text(textToDBreak.getText(), best.getText().getLanguage()), best.getDna());
+						decryptedTEXT.setText(decoded.getContentOfText());
+						bestFitMark.setText(best.getMark() + "");
+						decodedKeyField.setText(best.getDna().getKeyString());
+						generationNumber.setText(world.getWorldGeneration() + "");
+						updatePointsFor(best);
+					}
+				});
+				super.succeeded();
 
 			}
-			if(c.getDna() instanceof TranspositionKey){
-				return tCrypter;
-			}
+		};
+		progressBar.progressProperty().bind(task.progressProperty());
+
+		new Thread(task).start();
+
+	}
+
+	private void updatePointsFor(Creature c) {
+		pointsForList.clear();
+		pointsForList.addAll(c.getPoints().keySet());
+	}
+
+	private void initializeFitnessFunctions() {
+		fitnessFunctions.clear();
+		fitnessFunctions.add(fitnesserOnlyFrequentWord);
+		fitnessFunctions.add(fitnesserOnlyNgrams);
+	}
+
+	private Crypter crypterBasedDna(Creature c) {
+		if (c.getDna() instanceof SubstitutionKey) {
 			return sCrypter;
-		}
 
-	private void initWorld(){
+		}
+		if (c.getDna() instanceof TranspositionKey) {
+			return tCrypter;
+		}
+		return sCrypter;
+	}
+
+	private void initWorld() {
 		world.setTextToBreak(new Text(textToDBreak.getText()));
 	}
 
-	@FXML public void markText() {
+	@FXML
+	public void markText() {
 		initializeFitnessFunctions();
-		FitnessMaker selected=fitnessFunctions.get(fitnessByCombo.getSelectionModel().getSelectedIndex());
+		FitnessMaker selected = fitnessFunctions.get(markFunctions.getSelectionModel().getSelectedIndex());
 
-		Creature c=new Creature(new Text(cryptetTextArea.getText()), new TranspositionKey(5));
-
-
+		Creature c = new Creature(new Text(cryptetTextArea.getText()), new TranspositionKey(5));
 
 		selected.testCreatureInLife(c);
 
-		markLabel.setText(""+c.getMark());
-
-
-
-
-
-
-
+		markLabel.setText("" + c.getMark());
+		updatePointsFor(c);
 
 	}
 
-
-
-
-
-
-	private Key createRightKey(String x){
-		if(isInteger(x)){
-		return	new TranspositionKey(keyField.getText());
+	private Key createRightKey(String x) {
+		if (isInteger(x)) {
+			return new TranspositionKey(keyField.getText());
 		}
 		return new SubstitutionKey(keyField.getText().split(","));
 	}
-	private Crypter createRightCrypter(String x){
-		if(isInteger(x)){
-		return	tCrypter;
+
+	private Crypter createRightCrypter(String x) {
+		if (isInteger(x)) {
+			return tCrypter;
 		}
 		return sCrypter;
 	}
 
 	public static boolean isInteger(String s) {
-	    try {
-	        Integer.parseInt(s);
-	    } catch(NumberFormatException e) {
-	        return false;
-	    } catch(NullPointerException e) {
-	        return false;
-	    }
-	    // only got here if we didn't return false
-	    return true;
+		try {
+			Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			return false;
+		} catch (NullPointerException e) {
+			return false;
+		}
+		// only got here if we didn't return false
+		return true;
 	}
-
-
-
-
-
-
-
-
-
 
 }
